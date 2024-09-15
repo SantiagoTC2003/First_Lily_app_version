1813bd6c1e8387948f8914c4e715189335cb2fe3
admin <santiagotc0710@gmail.com>
2024-09-14 19:54:47 -0600

    App flower V1 commit

---
+++ Prueba_2_Movil/Prueba_2_Movil/MainPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
+using System.Collections.ObjectModel;
+using Xamarin.Essentials;
+using Plugin.BLE.Abstractions;
+using Plugin.BLE.Abstractions.Exceptions;
+
+
+namespace Prueba_2_Movil
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly IBluetoothLE ble;
+        private readonly IAdapter _adaptador;                                  // Clase del adaptador de bluetooth
+        private readonly ObservableCollection<IDevice> devicesList;            //Lista para almacenar dispositivos encontrados en el escaneo
+        private IDevice _dispositivoSelecionado;                               //Dispositivo para almacenar la informacion del dispositivo seleccionado
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ble = CrossBluetoothLE.Current;
+            _adaptador = CrossBluetoothLE.Current.Adapter;                     // Point _bluetoothAdapter to the current adapter on the phone
+            devicesList = new ObservableCollection<IDevice>();
+            _adaptador.DeviceDiscovered += (sender, args) =>
+            {
+                if (args.Device != null)   //&& !String.IsNullOrEmpty(args.Device.Name)
+                {
+                    devicesList.Add(args.Device);
+                }
+            };
+        }
+
+        private async Task<bool> PermissionsGrantedAsync()      // Function to make sure that all the appropriate approvals are in place
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+            if (status != PermissionStatus.Granted)
+            {
+                status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            }
+            return status == PermissionStatus.Granted;
+        }
+
+        public async void NavigateTo(object sender, EventArgs e)
+        {
+            LabelA.Text = "Connecting...";
+            _Indicador_Ocupado.IsVisible = _Indicador_Ocupado.IsRunning = !(Boton_Escaneo.IsEnabled = false);        // Swith the Isbusy Indicator on
+            if (_dispositivoSelecionado.State == DeviceState.Connected)                                                // Check first if we are already connected to the BLE Device 
+            {
+                await Navigation.PushAsync(new NavigationPage(new Menu()));
+            }
+            else
+            {
+                try
+                {
+                    var connectParameters = new ConnectParameters(true, true);
+                    await _adaptador.ConnectToDeviceAsync(_dispositivoSelecionado, connectParameters);          // if we are not connected, then try to connect to the BLE Device selected
+                    await Navigation.PushAsync(new NavigationPage(new Menu()));
+                    _Indicador_Ocupado.IsVisible = _Indicador_Ocupado.IsRunning = !(Boton_Escaneo.IsEnabled = true);
+                }
+                catch (DeviceConnectionException ex)
+                {
+                    await DisplayAlert("Error connecting", ex.Message.ToString(), "Retry");       // give an error message if it is not possible to connect
+                    _Indicador_Ocupado.IsVisible = _Indicador_Ocupado.IsRunning = !(Boton_Escaneo.IsEnabled = true);         // Switch off the busy indicator
+                    LabelA.Text = "Connection failed";
+                }
+            }
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            LabelA.Text = "Searching for devices";
+            _Indicador_Ocupado.IsVisible = _Indicador_Ocupado.IsRunning = !(Boton_Escaneo.IsEnabled = false);        // Swith the Isbusy Indicator on
+            ListaDispositivos.ItemsSource = null;                                                     // Empty the list of found BLE devices (in the GUI)
+            if (!await PermissionsGrantedAsync())                                                           // Make sure there is permission to use Bluetooth
+            {
+                await DisplayAlert("Permission required", "Application needs location permission", "OK");
+                _Indicador_Ocupado.IsVisible = _Indicador_Ocupado.IsRunning = !(_Indicador_Ocupado.IsEnabled = true);
+                return;
+            }
+            devicesList.Clear();                                                                           // Also clear the _gattDevices list
+            if (!_adaptador.IsScanning)                                                              // Make sure that the Bluetooth adapter is scanning for devices
+            {
+                await _adaptador.StartScanningForDevicesAsync();
+            }
+            foreach (var device in _adaptador.ConnectedDevices)                                      // Make sure BLE devices are added to the _gattDevices list
+                devicesList.Add(device);
+            ListaDispositivos.ItemsSource = devicesList;
+            await _adaptador.StopScanningForDevicesAsync();
+            _Indicador_Ocupado.IsVisible = _Indicador_Ocupado.IsRunning = !(Boton_Escaneo.IsEnabled = true);         // Switch off the busy indicator
+            LabelA.Text = "Please select Lily's device";
+        }
+
+        private async void ListaDispositivosEncontradosListView_ItemTapped(object sender, ItemTappedEventArgs e)   // Function that is run whenever a detected BLE device is selected
+        {
+            _dispositivoSelecionado = e.Item as IDevice;                                                       // The item selected is an IDevice (detected BLE device). Therefore we have to cast the selected item to an IDevice
+            lbl_Dispositvo_seleccionado.Text = Convert.ToString(_dispositivoSelecionado.NativeDevice + " " + _dispositivoSelecionado.IsConnectable);
+        }
+    }
+}
---
+++ Prueba_2_Movil/Prueba_2_Movil/Resultados.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prueba_2_Movil
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Resultados : ContentPage
+	{
+        public Resultados()
+        {
+            InitializeComponent();
+
+            string stream = "$$03928029111192831923871428031111111111##";
+
+            string Temperatura = stream.Substring(2, 4);
+            lblTemperatura.Text = "Temperature: " + Temperatura;
+
+            string Humedad = stream.Substring(6, 4);
+            lblHumedad.Text = "Humidity: " + Humedad;
+
+            string Audio = stream.Substring(11, 1);
+            if (Audio.Equals("1"))
+            {
+                lblAudio.Text = "Audio: activated";
+            }
+            else
+            {
+                lblAudio.Text = "Audio: desactited";
+            }
+
+
+            string Bluetooth = stream.Substring(12, 1);
+            if(Bluetooth.Equals("1"))
+            {
+                lblBluetooth.Text = "Bluetooth: activated";
+            }
+            else
+            {
+                lblBluetooth.Text = "Bluetooth: desactivated";
+            }
+
+            string Wifi = stream.Substring(13, 1);
+            if(Wifi.Equals("1")) 
+            { 
+                lblWifi.Text = "Wifi: activated";
+            }
+            else
+            {
+                lblWifi.Text = "Wifi: desactivated";
+            }
+
+            string Led = stream.Substring(30, 10);
+            lblLed.Text = "Led: 10/10 activated";
+
+        }
+
+		public string resultado { get; set; }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+			Navigation.PopAsync();
+        }
+    }
+}
